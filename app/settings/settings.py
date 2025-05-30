58a0762ebf4d788e89ccab6e2cf49e5d56fc9ad9
Ilgar Hasanzada <ilgarhasanzada@Ilgar-MacBook-Air.local>
2025-05-30 13:03:25 +0400

    dsa

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
-    "core.middlewares.ServerExpirationMiddleware",  
+    # "core.middlewares.ServerExpirationMiddleware",  
 
 ]
 
